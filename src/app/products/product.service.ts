d09c162ac1a0cef1eba979b0e01652efec041f8a
Rayal <prayal@dxc.com>
2021-08-16 09:18:13 +0530

    07-Combining Streams

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Observable, throwError } from 'rxjs';
+import { combineLatest, Observable, throwError } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
 import { Product } from './product';
 import { Supplier } from '../suppliers/supplier';
 import { SupplierService } from '../suppliers/supplier.service';
+import { ProductCategoryService } from "../product-categories/product-category.service";
 
 @Injectable({
   providedIn: 'root'
@@ -15,21 +16,40 @@ export class ProductService {
   private productsUrl = 'api/products';
   private suppliersUrl = this.supplierService.suppliersUrl;
 
+  // products$ = this.http.get<Product[]>(this.productsUrl)
+  //   .pipe(
+  //     map(products => 
+  //       products.map(product=> ({
+  //         ...product,
+  //         price: product.price * 1.5,
+  //         searchKey: [product.productName]
+  //       }) as Product)
+  //     ),
+  //     tap(data => console.log('Products: ', JSON.stringify(data))),
+  //     catchError(this.handleError)
+  //   );
   products$ = this.http.get<Product[]>(this.productsUrl)
     .pipe(
-      map(products => 
-        products.map(product=> ({
-          ...product,
-          price: product.price * 1.5,
-          searchKey: [product.productName]
-        }) as Product)
-      ),
       tap(data => console.log('Products: ', JSON.stringify(data))),
       catchError(this.handleError)
     );
 
-  constructor(private http: HttpClient,
-              private supplierService: SupplierService) { }
+  productsWithCategory$ = combineLatest([
+    this.products$,
+    this.productCategoryService.productCategories$
+  ]).pipe(
+    map(([products, categories])=>
+      products.map(product=> ({
+        ...product,
+        price: product.price * 1.5,
+        category: categories.find(c=>product.categoryId===c.id).name,
+        searchKey: [product.productName]
+      }) as Product)
+    )
+  );
+
+  constructor(private http: HttpClient, private supplierService: SupplierService,
+    private productCategoryService: ProductCategoryService) { }
 
   private fakeProduct(): Product {
     return {
